45075913f609636c963d487fc157b4ccea864c62
Erin Casserly <erin.casserly@jhpiego.org>
2015-06-29 12:41:34 -0400

    date2 update

--- date2.rb
+++ date2.rb
@@ -13,15 +13,47 @@ while response1 != "BYE" || response2 != "BYE" || response3 != "BYE"
 		if response1 == response.upcase
 		
 			puts "NO, NOT SINCE" + " " + (rand(14) + 1983).to_s + "!"
-			response = gets.chomp
 
 		else
 			puts "Hu!?!? Um - YA! I CAN'T HEAR YOU! THE MUSIC'S TOO LOUD!"
-			response = gets.chomp
 
 		end
 
+		if response1 == "BYE"
 
+			response2 == " "
+			response3 == " "
+		end
+
+	end
+
+
+	while response2 != "BYE"
+
+		response2 = gets.chomp
+
+		if response2 == response.upcase
+		
+			puts "NO, NOT SINCE" + " " + (rand(14) + 1983).to_s + "!"
+
+		else
+			puts "Hu!?!? Um - YA! I CAN'T HEAR YOU! THE MUSIC'S TOO LOUD!"
+
+		end
+	end
+
+	while response3 != "BYE"
+
+		response3 = gets.chomp
+
+		if response3 == response.upcase
+		
+			puts "NO, NOT SINCE" + " " + (rand(14) + 1983).to_s + "!"
+
+		else
+			puts "Hu!?!? Um - YA! I CAN'T HEAR YOU! THE MUSIC'S TOO LOUD!"
+
+		end
 	end
 
 	if response1 == "BYE" && response2 == "BYE" && response2 == "BYE"
